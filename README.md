77afde69cb9ae59726d0f3453529ea646d842dec
emirkugic <emir.kugic@hotmail.com>
2023-04-16 21:42:16 +0200

    fixing bug v1

--- README.md
+++
@@ -1,3 +0,0 @@
-# sorting-algorithm-visualizer-made-with-react
-
-It's a simple sorting algorithm visualizer with React.
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SortVisualizer from "./SortVisualizer/SortVisualizer";
-
-function App() {
-  return <SortVisualizer></SortVisualizer>;
-}
-
-export default App;
